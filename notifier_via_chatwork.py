70e98b89a2b8b1fd9bd86eb8b57b321e37887516
umihico <umihico_dummy@users.noreply.github.com>
2018-06-11 13:06:15 +0900

    bug fixed

--- notifier_via_chatwork.py
+++ notifier_via_chatwork.py
@@ -4,15 +4,18 @@ import pprint
 from passpacker import passwords
 
 
-def post_in_mychat(msg):
-    token = passwords['chatwork_api_key']
-    end_point_url = "https://api.chatwork.com/v2"
-    chat_id = passwords['chatwork_chatroom_number']
-    post_message_url = f'{end_point_url}/rooms/{chat_id}/messages'
-    headers = {'X-ChatWorkToken': token}
-    params = {'body': msg}
-    resp = requests.post(post_message_url, headers=headers, params=params)
-    pprint.pprint(resp.content)
+class ChatworkApi():
+    def __init__(self):
+        self.api_key = passwords['chatwork_api_key']
+        self.chat_id = passwords['chatwork_chatroom_number']
+
+    def post_in_mychat(self, msg):
+        end_point_url = "https://api.chatwork.com/v2"
+        post_message_url = f'{end_point_url}/rooms/{self.chat_id}/messages'
+        headers = {'X-ChatWorkToken': self.api_key}
+        params = {'body': msg}
+        resp = requests.post(post_message_url, headers=headers, params=params)
+        pprint.pprint(resp.content)
 
 
 if __name__ == '__main__':
