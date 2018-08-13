6274ff555d25b2eb6c468223de417b2163b8f94f
umihico <umihico_dummy@users.noreply.github.com>
2018-08-13 16:21:40 +0900

    update all

--- tor.py
+++ tor.py
@@ -2,10 +2,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import requests
-
-get = requests.get('http://httpbin.org/ip',
-                   proxies=dict(http='socks5://127.0.0.1:9050',
-                                https='socks5://127.0.0.1:9050')).text
-print(get)
-if __name__ == '__main__':
-    pass
+#
+# get = requests.get('http://httpbin.org/ip',
+#                    proxies=dict(http='socks5://127.0.0.1:9050',
+#                                 https='socks5://127.0.0.1:9050')).text
+# print(get)
+# if __name__ == '__main__':
+#     pass
